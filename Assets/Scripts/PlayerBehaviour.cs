ddddf8925974bb2f9c5483cc2c362d65dbd3eec6
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-06-09 17:49:19 +0800

    added an on Collision enter function and added more environment

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class PlayerBehaviout : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnInteract()
     {
-        
+        // This method will be called when the player interacts with an object
+        Debug.Log("Player interacted " + gameObject.name);
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        // This method will be called when the player collides with another object
+        Debug.Log("Player collided with " + collision.gameObject.name);
     }
 }
