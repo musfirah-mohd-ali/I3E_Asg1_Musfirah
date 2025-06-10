b425d6d1402756a2c7c68e87b6b964b627a1d5be
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-06-10 16:03:08 +0800

    made a simple collectible object and made the door work

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    private bool isOpen = false;
+    public void Interact()
+    {
+        Vector3 doorRotation = transform.eulerAngles;
+        if (!isOpen)
+        {
+            doorRotation.y += 90f;
+            isOpen = true;
+        }
+        else
+        {
+            doorRotation.y -= 90f;
+            isOpen = false;
+        }
+        transform.eulerAngles = doorRotation;
+
+    }
+}
+
+
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,14 +2,36 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    int score = 0;
+    int health = 100;
+    DoorBehaviour doorBehaviour;
+    bool canInteract = false;
     void OnInteract()
     {
         // This method will be called when the player interacts with an object
         Debug.Log("Player interacted " + gameObject.name);
+        if (canInteract)
+        {
+            if (doorBehaviour != null)
+            {
+                doorBehaviour.Interact();
+            }
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
         // This method will be called when the player collides with another object
         Debug.Log("Player collided with " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Door"))
+        {
+            doorBehaviour = collision.gameObject.GetComponent<DoorBehaviour>();
+            canInteract = true;
+        }
+        else if (collision.gameObject.CompareTag("Collectible"))
+        {
+            score += 10;
+            Debug.Log("Collected: " + gameObject.name);
+            Destroy(collision.gameObject);
+        }
     }
 }
