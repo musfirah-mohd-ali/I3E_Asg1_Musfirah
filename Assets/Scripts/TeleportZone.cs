6ad03031cc287477ca609f2e2c8176d2c5b63908
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-06-12 18:30:57 +0800

    added the second last challenge and added all the instructions

--- Assets/Scripts/TeleportZone.cs
+++ Assets/Scripts/TeleportZone.cs
@@ -4,15 +4,28 @@ public class TeleportZone : MonoBehaviour
 {
     [SerializeField]
     Transform teleportTarget;
-
+    bool isPressed = false;
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Pushable"))
+        {
+            isPressed = true;
+            Debug.Log("Pushable object entered teleport zone!");
+        }
+        if (other.CompareTag("Player") && isPressed)
         {
             Debug.Log("Player entered teleport zone!");
             other.transform.position = teleportTarget.position;
             Physics.SyncTransforms(); // Ensure physics updates immediately
             Debug.Log("Player teleported!");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pushable"))
+        {
+            isPressed = false;
+            Debug.Log("Pushable object exited teleport zone!");
         }
     }
 }
